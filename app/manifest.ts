8ec28703a807a7fd57ee885c58134e84755e87fd
fadiu007-creator <fatmirsejdiu007@gmail.com>
2026-08-31 19:15:01 +0200

    Add installable PWA manifest

---
+++ app/manifest.ts
@@ -0,0 +1,2 @@
+import type { MetadataRoute } from 'next'
+export default function manifest(): MetadataRoute.Manifest{return {name:'Zikos Real Estate',short_name:'Zikos',description:'Real estate marketplace for Kosovo',start_url:'/',display:'standalone',background_color:'#ffffff',theme_color:'#123d34',icons:[]}}
